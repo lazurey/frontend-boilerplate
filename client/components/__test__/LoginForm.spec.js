aaae805e589fcbb8048321e56855148438dd3829
Sause <lazureys@gmail.com>
2016-03-14 08:30:15 +0800

    update test

--- client/components/__test__/LoginForm.spec.js
+++ client/components/__test__/LoginForm.spec.js
@@ -4,7 +4,7 @@ import { shallow } from 'enzyme'
 import LoginForm from '../LoginForm'
 
 describe('<LoginForm />', () => {
-  it('should render a login form', () => {
+  it('should render a login form with an email field', () => {
     const wrapper = shallow(<LoginForm />)
     wrapper.find('input').length.should.be.equal(1)
   })
